fba2127f862d88712426c791a891e987485a30ed
Dipanshu0919 <108180019+Dipanshu0919@users.noreply.github.com>
2025-01-03 18:30:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
+import os, re, sys, ccxt, time
 from telethon import TelegramClient, events
 
-# Use your own values from my.telegram.org
-api_id = 'YOUR_API_ID'
-api_hash = 'YOUR_API_HASH'
-bot_token = 'YOUR_BOT_TOKEN'
+api_id = 25895085
+api_hash = "4d83e959108956d7c0b05bd8f52f54b5"
+bot_token = os.environ.get("BOT_TOKEN")
 
-# Create the client and connect
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-# Define a handler for new messages
-@client.on(events.NewMessage)
-async def handle_new_message(event):
-    # Print the message to the console
-    print(event.message.message)
+@client.on(events.NewMessage(pattern="start"))
+async def start(event):
+    ex = ccxt.coinmarketcap()
+    while true:
+        n = ex.fetch_ticker("BTC/USDT)
+        await event.reply(n["last"])
+        time.sleep(15)
+        
 
-    # Reply to the message
-    await event.reply('Hello! This is a Telethon bot.')
-
-# Start the client
 client.run_until_disconnected()
