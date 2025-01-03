dceacb31234fdf5ba8e043ceaa9df86a2fbfd4ca
Dipanshu0919 <108180019+Dipanshu0919@users.noreply.github.com>
2025-01-03 19:10:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-import os, re, sys, ccxt, time
+import os, re, sys, ccxt, time, threading
 from telethon import TelegramClient, events
+from flask import Flask
+from datetime import datetime, timedelta
 
 api_id = 25895085
 api_hash = "4d83e959108956d7c0b05bd8f52f54b5"
@@ -7,20 +9,36 @@ bot_token = os.environ.get("BOT_TOKEN")
 
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
+app = Flask(__name__)
+
+run = True
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'Healthy', 200
+
 @client.on(events.NewMessage(pattern="/start"))
 async def start(event):
-    run = True
+    k = await event.reply("`Showing BTC Price...`")
+    global run
     ex = ccxt.mexc()
     while run:
         n = ex.fetch_ticker("BTC/USDT")
-        lp = str(n["last"])
-        await event.reply(lp)
+        time = (datetime.utcnow() + timedelta(hours=5, minutes=30)).strfttime("**Date:** `%d %B %Y` \n**Time:** `%H:%M:%S` ")
+        lp = f"**BTC Price:** `{str(n["last"])}` \n\n**Last Update:**\n"
+        await k.edit(lp)
         time.sleep(15)
 
 @client.on(events.NewMessage(pattern="/stop"))
-async def start(event):
+async def stop(event):
     global run
     run = False
-        
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)
+
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.daemon = True
+flask_thread.start()
 
 client.run_until_disconnected()
